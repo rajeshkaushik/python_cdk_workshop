003c2f002bc042be628a13716f6666bf7eedf8ee
rajeshkaushik <rajesh.kaushik15@gmail.com>
2023-06-01 13:15:22 +0530

    first commit with successfull deploy to us-west-2

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import aws_cdk as cdk
 
 from python_cdk_workshop.python_cdk_workshop_stack import PythonCdkWorkshopStack
 
-
 app = cdk.App()
-PythonCdkWorkshopStack(app, "python-cdk-workshop")
+PythonCdkWorkshopStack(app, "python-cdk-workshop", env=cdk.Environment(region="us-west-2"))
 
 app.synth()
